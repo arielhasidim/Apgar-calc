3783e92d76f9400936db9cdd66385d98789bb0b2
Ariel <23483971+arielhasidim@users.noreply.github.com>
2024-12-28 19:43:23 +0200

    Updates

---
+++ assets/card-CQLekRRq.js
@@ -0,0 +1,16 @@
+import{c as o,r as t,j as r,a as d}from"./index-BDCga6LR.js";/**
+ * @license lucide-react v0.396.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const f=o("Copy",[["rect",{width:"14",height:"14",x:"8",y:"8",rx:"2",ry:"2",key:"17jyea"}],["path",{d:"M4 16c-1.1 0-2-.9-2-2V4c0-1.1.9-2 2-2h10c1.1 0 2 .9 2 2",key:"zix9uf"}]]);/**
+ * @license lucide-react v0.396.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const m=o("Play",[["polygon",{points:"6 3 20 12 6 21 6 3",key:"1oa8hb"}]]);/**
+ * @license lucide-react v0.396.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const C=o("Save",[["path",{d:"M15.2 3a2 2 0 0 1 1.4.6l3.8 3.8a2 2 0 0 1 .6 1.4V19a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2z",key:"1c8476"}],["path",{d:"M17 21v-7a1 1 0 0 0-1-1H8a1 1 0 0 0-1 1v7",key:"1ydtos"}],["path",{d:"M7 3v4a1 1 0 0 0 1 1h7",key:"t51u73"}]]),c=t.forwardRef(({className:a,...e},s)=>r.jsx("div",{ref:s,className:d("rounded-lg border bg-card text-card-foreground shadow-sm",a),...e}));c.displayName="Card";const n=t.forwardRef(({className:a,...e},s)=>r.jsx("div",{ref:s,className:d("flex flex-col space-y-1.5 p-6",a),...e}));n.displayName="CardHeader";const i=t.forwardRef(({className:a,...e},s)=>r.jsx("h3",{ref:s,className:d("text-2xl font-semibold leading-none tracking-tight",a),...e}));i.displayName="CardTitle";const p=t.forwardRef(({className:a,...e},s)=>r.jsx("p",{ref:s,className:d("text-sm text-muted-foreground",a),...e}));p.displayName="CardDescription";const l=t.forwardRef(({className:a,...e},s)=>r.jsx("div",{ref:s,className:d("p-6 pt-0",a),...e}));l.displayName="CardContent";const y=t.forwardRef(({className:a,...e},s)=>r.jsx("div",{ref:s,className:d("flex items-center p-6 pt-0",a),...e}));y.displayName="CardFooter";export{c as C,m as P,C as S,n as a,i as b,l as c,f as d};
